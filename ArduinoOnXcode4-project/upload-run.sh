7ac0faf5f4511f2a4e00446067ffd5f2ff1a0190
Jérôme Lebel <jerome@fotonauts.com>
2012-01-26 22:19:43 +0100

    using the variables to make it working

--- ArduinoOnXcode4-project/upload-run.sh
+++ ArduinoOnXcode4-project/upload-run.sh
@@ -6,9 +6,13 @@
 #  Created by Jérôme Lebel on 25/01/12.
 #  Copyright (c) 2012 Fotonauts. All rights reserved.
 
-APPLICATION_PATH=$1
-HEX_PATH=$2
-BOARDS_TXT_PATH=$3
+ARDUINO_APP_PATH="$1"
+HEX_PATH="$2"
+BOARDS_TXT_PATH="$3"
+BUILD_DIR="$4"
+PORT=`ls "$5"`
+HARDWARE_NAME="$6"
 
 
-/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin/avrdude -C/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/etc/avrdude.conf -patmega2560 -P/dev/tty.usbmodem411 -c stk500v2 -b115200 -D -Uflash:w:/Users/jerome/Library/Developer/Xcode/DerivedData/ArduinoOnXcode-arkyvqhucytijbdxryabqdezjdoi/Build/Products/main.hex:i
+
+"${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/bin/avrdude" "-C${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/etc/avrdude.conf" "-p${HARDWARE_NAME}" "-P${PORT}" -c stk500v2 -b115200 -D "-Uflash:w:${BUILD_DIR}main.hex:i"

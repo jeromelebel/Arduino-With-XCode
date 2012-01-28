b612158f893fbf61cf0d3fad944c58903fdb9d30
Jérôme Lebel <jerome@fotonauts.com>
2012-01-28 23:19:57 +0100

    getting the upload rate into the script

--- ArduinoOnXcode4-project/upload-run.sh
+++ ArduinoOnXcode4-project/upload-run.sh
@@ -3,13 +3,16 @@
 #  upload-run.sh
 #  ArduinoOnXcode
 
+echo "$@"
+echo " "
+
 ARDUINO_APP_PATH="$1"
-HEX_PATH="$2"
-BOARDS_TXT_PATH="$3"
-BUILD_DIR="$4"
-PORT=`ls "$5"`
-HARDWARE_NAME="$6"
+BOARDS_TXT_PATH="$2"
+BUILD_DIR="$3"
+PORT=`ls "$4"`
+HARDWARE_NAME="$5"
+BOARD_NAME="$6"
 
+UPLOAD_RATE=`grep -e"${BOARD_NAME}.upload.speed=" "${BOARDS_TXT_PATH}" | sed 's/'"${BOARD_NAME}.upload.speed="'//'`
 
-
-"${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/bin/avrdude" "-C${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/etc/avrdude.conf" "-p${HARDWARE_NAME}" "-P${PORT}" -c stk500v2 -b115200 -D "-Uflash:w:${BUILD_DIR}main.hex:i"
+"${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/bin/avrdude" "-C${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/etc/avrdude.conf" "-p${HARDWARE_NAME}" "-P${PORT}" -c stk500v2 "-b${UPLOAD_RATE}" -D "-Uflash:w:${BUILD_DIR}main.hex:i"

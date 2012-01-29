005bb5a6633c2089854bc397225d649307a9edcf
Jérôme Lebel <jerome@fotonauts.com>
2012-01-29 12:03:38 +0100

    should be able to run from xcode

--- ArduinoOnXcode4-project/upload-run.sh
+++ ArduinoOnXcode4-project/upload-run.sh
@@ -16,3 +16,4 @@ BOARD_NAME="$6"
 UPLOAD_RATE=`grep -e"${BOARD_NAME}.upload.speed=" "${BOARDS_TXT_PATH}" | sed 's/'"${BOARD_NAME}.upload.speed="'//'`
 
 "${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/bin/avrdude" "-C${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/etc/avrdude.conf" "-p${HARDWARE_NAME}" "-P${PORT}" -c stk500v2 "-b${UPLOAD_RATE}" -D "-Uflash:w:${BUILD_DIR}main.hex:i"
+screen "${PORT}" "${UPLOAD_RATE}"

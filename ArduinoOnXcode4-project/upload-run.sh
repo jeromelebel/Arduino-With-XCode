48143882ca7e950c10fe580ad87318aeb634abac
Jérôme Lebel <jerome@fotonauts.com>
2012-02-10 22:28:05 +0100

    can upload from xcode with the 'Run' command

--- ArduinoOnXcode4-project/upload-run.sh
+++ ArduinoOnXcode4-project/upload-run.sh
@@ -4,16 +4,23 @@
 #  ArduinoOnXcode
 
 echo "$@"
-echo " "
+echo "-- "
 
 ARDUINO_APP_PATH="$1"
 BOARDS_TXT_PATH="$2"
 BUILD_DIR="$3"
-PORT=`ls "$4"`
+PORT=`ls $4`
 HARDWARE_NAME="$5"
 BOARD_NAME="$6"
+HARDWARE_PATH="$7"
 
 UPLOAD_RATE=`grep -e"${BOARD_NAME}.upload.speed=" "${BOARDS_TXT_PATH}" | sed 's/'"${BOARD_NAME}.upload.speed="'//'`
+PROTOCOL=`grep -e"${BOARD_NAME}.upload.protocol=" "${BOARDS_TXT_PATH}" | sed 's/'"${BOARD_NAME}.upload.protocol="'//'`
+MCU=`grep -e"${BOARD_NAME}.build.mcu=" "${BOARDS_TXT_PATH}" | sed 's/'"${BOARD_NAME}.build.mcu="'//'`
 
-"${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/bin/avrdude" "-C${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/etc/avrdude.conf" "-p${HARDWARE_NAME}" "-P${PORT}" -c stk500v2 "-b${UPLOAD_RATE}" -D "-Uflash:w:${BUILD_DIR}main.hex:i"
+if [ "${PROTOCOL}" = "" ] ; then
+  PROTOCOL="stk500"
+fi
+
+"${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/bin/avrdude" "-C${ARDUINO_APP_PATH}/Contents/Resources/Java/hardware/tools/avr/etc/avrdude.conf" "-p${MCU}" "-P${PORT}" "-c${PROTOCOL}" "-b${UPLOAD_RATE}" -D "-Uflash:w:${BUILD_DIR}/main.hex:i"
 screen "${PORT}" "${UPLOAD_RATE}"
